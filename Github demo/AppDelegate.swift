01659724e4808030b7d6afe31f8fc1b5daec5cfa
Chloe <s5105165@bournemouth.ac.uk>
2018-11-20 15:37:57 +0000

    remove comments
    appdelegate

--- Github demo/AppDelegate.swift
+++ Github demo/AppDelegate.swift
@@ -1,10 +1,3 @@
-//
-//  AppDelegate.swift
-//  Github demo
-//
-//  Created by Chloe Young (s5105165) on 20/11/2018.
-//  Copyright © 2018 Chloe Young (s5105165). All rights reserved.
-//
 
 import UIKit
 
